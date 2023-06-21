edd46233150c261d2c973b829ece138f550c10d1
Marc13 <marcusolsson123@hotmail.com>
2023-06-21 17:23:06 +0200

    updated website for Ultima

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -5,20 +5,27 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      <nav className="navbar">
+        <div className="navbar-logo">
+          <img src={logo} alt="logo" />
+        </div>
+        <div className="navbar-links">
+          <a href="/">Home</a>
+          <a href="https://discord.com/api/oauth2/authorize?client_id=1113874631489753148&permissions=8&scope=applications.commands%20bot">Invite</a>
+          <a href="https://discord.gg/HHxRfnYEZb">Discord Server</a>
+        </div>
+      </nav>
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Welcome to Ultima!
+        <p className="description-text">
+          Ultima
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p className="ultima-text">
+          The most modern discord bot that has economy, moderation, automod, leveling, fun commands, useful tools, ticketing, ChatGPT and much more!
+        </p>
       </header>
+      <footer className="footer">
+        Developed by the Ultima team
+      </footer>
     </div>
   );
 }
